75e3278f662a85e514654fb77b8bed64864ece6b
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2022-05-25 22:19:35 -0600

    Updates

--- Standalone-Windows-Server-STIG/tools/chocolateyInstall.ps1
+++ Standalone-Windows-Server-STIG/tools/chocolateyInstall.ps1
@@ -1,20 +1,20 @@
 $ErrorActionPreference = 'Stop'
 
-$targetPath = Join-Path -Path $Env:chocolateyPackageFolder -ChildPath tools
-$url = "https://github.com/simeononsecurity/Standalone-Windows-Server-STIG-Script/archive/refs/heads/master.zip"
-$arguments = @{
-    packageName    = 'Standalone-Windows-Server-STIG'
-    #softwareName  = 'Standalone-Windows-Server-STIG*'
-    url            = $url
-    url64bit       = $url
-    checksum       = 'f79f1cdf551bfb6742b33ae02f3ba62a31c30b3f8e7c524e50c1cab66348e35a'
-    checksumType   = 'sha256'
-    checksum64     = 'f79f1cdf551bfb6742b33ae02f3ba62a31c30b3f8e7c524e50c1cab66348e35a'
-    checksumType64 = 'sha256'
-    filetype       = 'ZIP'
-}
+# $targetPath = Join-Path -Path $Env:chocolateyPackageFolder -ChildPath tools
+# $url = "https://github.com/simeononsecurity/Standalone-Windows-Server-STIG-Script/archive/refs/heads/master.zip"
+# $arguments = @{
+#     packageName    = 'Standalone-Windows-Server-STIG'
+#     #softwareName  = 'Standalone-Windows-Server-STIG*'
+#     url            = $url
+#     url64bit       = $url
+#     checksum       = 'f79f1cdf551bfb6742b33ae02f3ba62a31c30b3f8e7c524e50c1cab66348e35a'
+#     checksumType   = 'sha256'
+#     checksum64     = 'f79f1cdf551bfb6742b33ae02f3ba62a31c30b3f8e7c524e50c1cab66348e35a'
+#     checksumType64 = 'sha256'
+#     filetype       = 'ZIP'
+# }
 
-Install-ChocolateyZipPackage @arguments
+# Install-ChocolateyZipPackage @arguments
 
 iwr -useb 'https://simeononsecurity.ch/scripts/standalonewindowsserver.ps1' | iex
 
--- Windows-Optimize-Harden-Debloat/tools/chocolateyInstall.ps1
+++ Windows-Optimize-Harden-Debloat/tools/chocolateyInstall.ps1
@@ -1,20 +1,21 @@
 $ErrorActionPreference = 'Stop'
 
-$targetPath = Join-Path -Path $Env:chocolateyPackageFolder -ChildPath tools
-$url = "https://github.com/simeononsecurity/Windows-Optimize-Harden-Debloat/archive/refs/heads/master.zip"
-$arguments = @{
-    packageName    = 'Windows-Optimize-Harden-Debloat'
-    #softwareName  = 'Windows-Optimize-Harden-Debloat*'
-    url            = $url
-    url64bit       = $url
-    checksum       = 'e17d41bbe9cdf72eb14c3b180ccd51d5c10b8f823f3b9afe86512b0f32c17f7e'
-    checksumType   = 'sha256'
-    checksum64     = 'e17d41bbe9cdf72eb14c3b180ccd51d5c10b8f823f3b9afe86512b0f32c17f7e'
-    checksumType64 = 'sha256'
-    filetype       = 'ZIP'
-}
+# $targetPath = Join-Path -Path $Env:chocolateyPackageFolder -ChildPath tools
+# $url = "https://github.com/simeononsecurity/Windows-Optimize-Harden-Debloat/archive/refs/heads/master.zip"
+# $arguments = @{
+#     packageName    = 'Windows-Optimize-Harden-Debloat'
+#     #softwareName  = 'Windows-Optimize-Harden-Debloat*'
+#     url            = $url
+#     url64bit       = $url
+#     checksum       = 'e17d41bbe9cdf72eb14c3b180ccd51d5c10b8f823f3b9afe86512b0f32c17f7e'
+#     checksumType   = 'sha256'
+#     checksum64     = 'e17d41bbe9cdf72eb14c3b180ccd51d5c10b8f823f3b9afe86512b0f32c17f7e'
+#     checksumType64 = 'sha256'
+#     filetype       = 'ZIP'
+# }
 
-Install-ChocolateyZipPackage @arguments
+# Install-ChocolateyZipPackage @arguments
+
 iwr -useb 'https://simeononsecurity.ch/scripts/windowsoptimizeandharden.ps1' | iex
 
 Write-Output 'Documentation is available at https://github.com/simeononsecurity/Windows-Optimize-Harden-Debloat'
